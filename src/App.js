07bdbfd0401b805878be5f536d0085faf31014f4
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-07-20 17:50:28 -0500

    added with router and album and artist view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import {useEffect, useState } from 'react'
+import {useEffect, useState, Fragment } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
-import Searchbar from './components/Searchbar'
-import { DataContext } from './context/DataContext'
+import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 
 function App() {
 	let [search, setSearch] = useState('')
@@ -33,13 +35,22 @@ function App() {
 
 	return (
 		<div>
-			<Searchbar handleSearch = {handleSearch}/>
-			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+		{message}
+			<Router>
+				<Routes>
+					<Route path="/" element={
+						<Fragment>
+							<SearchBar handleSearch = {handleSearch}/>
+							<Gallery data={data} />
+						</Fragment>
+					} />
+					<Route path="/album/:id" element={<AlbumView />} />
+					<Route path="/artist/:id" element={<ArtistView />} />
+				</Routes>
+			</Router>
 		</div>
-  	);
+	)
+	
 }
 
 export default App;
