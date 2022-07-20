a84acd06ee1a9ff875062058398ab43e3b132059
Rebecca Coburn <rebeccacoburn@Rebeccas-iMac.local>
2022-07-20 18:44:30 -0500

    working

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,10 +3,9 @@ import { useContext } from 'react'
 import { DataContext } from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
-function Gallery(){
-    const data = useContext(DataContext)
+function Gallery(props){
     
-    const display = data.map((item,index) => {
+    const display = props.data.map((item,index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,19 +1,17 @@
 // Seachbar.js
 import { useState } from 'react'
 
-function SearchBar(props) {
+function SearchBar(props){
+    // We can comment out our searchTerm state variable as we are not using it!
     let [searchTerm, setSearchTerm] = useState('')
 
     return (
-        <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
-
-            <input type="text" placeholder="Enter a search term here" onChange={
-                (e) => setSearchTerm(e.target.value)
-            }/>
-
-            <input type="submit" />
-
-        </form>
+            <form onSubmit={(e) => props.handleSearch(e, searchTerm)}>
+                <input type="text" placeholder="Search Here"
+                    onChange={(e) => setSearchTerm(e.target.value)
+                    } />
+                <input type="submit" />
+            </form>
     )
 }
 
